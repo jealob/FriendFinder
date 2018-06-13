204a6349ca6bf27d0b90a9c4f797ebb123994a33
jealob <jealob@gmail.com>
2018-06-13 16:16:20 -0500

    Updates fix for empty friends arra

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,68 +1,68 @@
 let friendsArray = [
-    {
-        name: "John Smith",
-        photo: "https://avatars0.githubusercontent.com/u/32347126?v=4",
-        scores: [
-            "4",
-            "3",
-            "4",
-            "3",
-            "4",
-            "2",
-            "5",
-            "5",
-            "4",
-            "2"
-        ]
-    },
-    {
-        name: "Augusta May",
-        photo: "https://avatars0.githubusercontent.com/u/32347126?v=4",
-        scores: [
-            "3",
-            "2",
-            "3",
-            "2",
-            "3",
-            "1",
-            "1",
-            "4",
-            "4",
-            "4"
-        ]
-    },
-    {
-        name: "Brian Scott",
-        photo: "https://avatars0.githubusercontent.com/u/32347126?v=4",
-        scores: [
-            "2",
-            "3",
-            "4",
-            "3",
-            "1",
-            "3",
-            "1",
-            "1",
-            "2",
-            "4"
-        ]
-    },
-    {
-        name: "Jill Adams",
-        photo: "https://avatars0.githubusercontent.com/u/32347126?v=4",
-        scores: [
-            "4",
-            "3",
-            "2",
-            "3",
-            "5",
-            "3",
-            "5",
-            "5",
-            "4",
-            "2"
-        ]
-    }
+    // {
+    //     name: "John Smith",
+    //     photo: "https://avatars0.githubusercontent.com/u/32347126?v=4",
+    //     scores: [
+    //         "4",
+    //         "3",
+    //         "4",
+    //         "3",
+    //         "4",
+    //         "2",
+    //         "5",
+    //         "5",
+    //         "4",
+    //         "2"
+    //     ]
+    // },
+    // {
+    //     name: "Augusta May",
+    //     photo: "https://avatars0.githubusercontent.com/u/32347126?v=4",
+    //     scores: [
+    //         "3",
+    //         "2",
+    //         "3",
+    //         "2",
+    //         "3",
+    //         "1",
+    //         "1",
+    //         "4",
+    //         "4",
+    //         "4"
+    //     ]
+    // },
+    // {
+    //     name: "Brian Scott",
+    //     photo: "https://avatars0.githubusercontent.com/u/32347126?v=4",
+    //     scores: [
+    //         "2",
+    //         "3",
+    //         "4",
+    //         "3",
+    //         "1",
+    //         "3",
+    //         "1",
+    //         "1",
+    //         "2",
+    //         "4"
+    //     ]
+    // },
+    // {
+    //     name: "Jill Adams",
+    //     photo: "https://avatars0.githubusercontent.com/u/32347126?v=4",
+    //     scores: [
+    //         "4",
+    //         "3",
+    //         "2",
+    //         "3",
+    //         "5",
+    //         "3",
+    //         "5",
+    //         "5",
+    //         "4",
+    //         "2"
+    //     ]
+    // }
 ];
 
 // exports array to make it accessible to routing files
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,12 +2,14 @@
 // Requires the friends.js file to get the friend array data
 let friendsData = require("../data/friends.js");
 
-// API endpoint for all friends data
+
 module.exports = function (app) {
+    // get API endpoint for friends data array
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
         console.log(friendsData);
     });
+    // Post to API endpoint for friends data array
     app.post("/api/friends", function (req, res) {
         let friendFinder = req.body;
         let match = {
@@ -18,8 +20,10 @@ module.exports = function (app) {
         for (friend in friendsData) {
             let difference = null;
             for (score in friendsData[friend].scores) {
+                // Determines the closeest match
                 difference += Math.abs(friendFinder.scores[score] - friendsData[friend].scores[score]);
             }
+            // Insert the closest match from the friends data array 
             if (!match.matchRating || difference < match.matchRating) {
                 match.name = friendsData[friend].name;
                 match.photo = friendsData[friend].photo;
@@ -27,11 +31,15 @@ module.exports = function (app) {
             }
         };
         if (match.name) {
-            console.log(friendFinder);
             res.json(match);
         }
         else {
-            console.log(match);
+            match = {
+                name: "No Match at the time",
+                photo: null,
+                matchRating: ""
+            }
+            res.json(match);
         }
         friendsData.push(req.body);
     });
