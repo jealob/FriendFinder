2f3c3975e626487fc31fe9a8361f172ef6ed2fc9
jealob <jealob@gmail.com>
2018-06-13 22:45:35 -0500

    adds images in friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,68 +1,68 @@
 let friendsArray = [
-    // {
-    //     name: "John Smith",
-    //     photo: "https://avatars0.githubusercontent.com/u/32347126?v=4",
-    //     scores: [
-    //         "4",
-    //         "3",
-    //         "4",
-    //         "3",
-    //         "4",
-    //         "2",
-    //         "5",
-    //         "5",
-    //         "4",
-    //         "2"
-    //     ]
-    // },
-    // {
-    //     name: "Augusta May",
-    //     photo: "https://avatars0.githubusercontent.com/u/32347126?v=4",
-    //     scores: [
-    //         "3",
-    //         "2",
-    //         "3",
-    //         "2",
-    //         "3",
-    //         "1",
-    //         "1",
-    //         "4",
-    //         "4",
-    //         "4"
-    //     ]
-    // },
-    // {
-    //     name: "Brian Scott",
-    //     photo: "https://avatars0.githubusercontent.com/u/32347126?v=4",
-    //     scores: [
-    //         "2",
-    //         "3",
-    //         "4",
-    //         "3",
-    //         "1",
-    //         "3",
-    //         "1",
-    //         "1",
-    //         "2",
-    //         "4"
-    //     ]
-    // },
-    // {
-    //     name: "Jill Adams",
-    //     photo: "https://avatars0.githubusercontent.com/u/32347126?v=4",
-    //     scores: [
-    //         "4",
-    //         "3",
-    //         "2",
-    //         "3",
-    //         "5",
-    //         "3",
-    //         "5",
-    //         "5",
-    //         "4",
-    //         "2"
-    //     ]
-    // }
+    {
+        name: "John Smith",
+        photo: "https://petapixel.com/assets/uploads/2015/01/normal-londoners-wearing-not-very-normal-clothes-823-body-image-1420628732.jpg",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "3",
+            "4",
+            "2",
+            "5",
+            "5",
+            "4",
+            "2"
+        ]
+    },
+    {
+        name: "Augusta May",
+        photo: "http://twistedsifter.files.wordpress.com/2013/09/national-beard-and-mustache-championships-2013-new-orleands-by-greg-anderson-14.jpg?w=539&h=720&zoom=2",
+        scores: [
+            "3",
+            "2",
+            "3",
+            "2",
+            "3",
+            "1",
+            "1",
+            "4",
+            "4",
+            "4"
+        ]
+    },
+    {
+        name: "Brian Scott",
+        photo: "https://twistedsifter.files.wordpress.com/2013/09/national-beard-and-mustache-championships-2013-new-orleands-by-greg-anderson-8.jpg",
+        scores: [
+            "2",
+            "3",
+            "4",
+            "3",
+            "1",
+            "3",
+            "1",
+            "1",
+            "2",
+            "4"
+        ]
+    },
+    {
+        name: "Jill Adams",
+        photo: "https://petapixel.com/assets/uploads/2015/01/normal-londoners-wearing-not-very-normal-clothes-823-body-image-1420629441.jpg",
+        scores: [
+            "4",
+            "3",
+            "2",
+            "3",
+            "5",
+            "3",
+            "5",
+            "5",
+            "4",
+            "2"
+        ]
+    }
 ];
 
 // exports array to make it accessible to routing files
